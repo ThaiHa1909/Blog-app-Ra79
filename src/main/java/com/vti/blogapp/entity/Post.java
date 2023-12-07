635ba25ffceb87b54962b8ff66a520e58d511440
misaki.janet <misaki.janet@gmail.com>
2023-12-07 22:08:35 +0900

    [Add] Comment entity

--- src/main/java/com/vti/blogapp/entity/Post.java
+++ src/main/java/com/vti/blogapp/entity/Post.java
@@ -8,6 +8,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @Setter
 @ToString
@@ -35,4 +37,8 @@ public class Post {
     @Column(name = "update_at", nullable = false)
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
 }
