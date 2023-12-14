202b28b042fa4eed1204555bc70a1ebf87d040df
misaki.janet <misaki.janet@gmail.com>
2023-12-14 23:47:06 +0900

    [Add] Config OpenAPI

---
+++ src/main/java/com/vti/blogapp/configuration/OpenApiConfiguration.java
@@ -0,0 +1,43 @@
+package com.vti.blogapp.configuration;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Blog Application REST APIs",
+                description = "Blog Application REST APIs Documentation",
+                termsOfService = "https://github.com/ThaiHa1909/Blog-app-Ra79",
+                contact = @Contact(
+                        name = "Nguyen Thi Thai Ha",
+                        url = "https://github.com/ThaiHa1909",
+                        email = "misaki.janet@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                version = "v1.0"
+        ),
+        servers = @Server(
+                description = "Local ENV",
+                url = "http://localhost:8080"
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Blog Application Github",
+                url = "https://github.com/khoa-omega/blog-app"
+        )
+//
+//@SecurityScheme(
+//        type = SecuritySchemeType.HTTP,
+//        name = "Bearer Authentication",
+//        in = SecuritySchemeIn.HEADER,
+//        scheme = "bearer",
+//        bearerFormat = "JWT"
+)
+public class OpenApiConfiguration {
+}
